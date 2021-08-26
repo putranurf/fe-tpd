0ac60ebf690af450f8e30a6fa9e0fed6f45006ce
Putra Nurfajar <putra@pindad.com>
2021-08-26 16:20:41 +0700

    [FEATURE] Dashboard

--- src/navigation/PublicRoute.js
+++ src/navigation/PublicRoute.js
@@ -8,30 +8,14 @@ import SLUGS from "../utils/slugs";
 // const PesananComponent = lazy(() => import('../pages/Pesanan'));
 
 import Auth from "../pages/Home/components/Authentication/auth";
+import Dashboard from "../pages/Home/components/Dashboard/dashboard";
 
 function PublicRoute() {
   return (
     <Router>
       <Switch>
-        <Route path={SLUGS.login} component={Auth}>
-          {/* <Route exact path={SLUGS.overviewTwo} render={() => <div>overviewTwo</div>} />
-                <Route exact path={SLUGS.overviewThree} render={() => <div>overviewThree</div>} />
-                <Route exact path={SLUGS.overview} render={() => <div>overview</div>} />
-                <Route exact path={SLUGS.tickets} render={() => <div>tickets</div>} />
-                <Route exact path={SLUGS.ideasTwo} render={() => <div>ideasTwo</div>} />
-                <Route exact path={SLUGS.ideasThree} render={() => <div>ideasThree</div>} />
-                <Route exact path={SLUGS.ideas} render={() => <div>ideas</div>} />
-                <Route exact path={SLUGS.contacts} render={() => <div>contacts</div>} /> */}
-          {/* <Route exact path={SLUGS.produk} component={ProdukComponent} />
-                <Route exact path={SLUGS.kategori} render={() => <div>Kategori</div>} />
-                <Route exact path={SLUGS.user} render={() => <div>User</div>} />
-                <Route exact path={SLUGS.voucher} render={() => <div>Voucher</div>} />
-                <Route exact path={SLUGS.pesanan} component={PesananComponent} />
-                <Route exact path={SLUGS.tracking} render={() => <div>Tracking</div>} />
-                <Route exact path={SLUGS.payment} render={() => <div>Payment</div>} />
-                <Route exact path={SLUGS.subscription} render={() => <div>subscription</div>} />
-                <Redirect to={SLUGS.dashboard} /> */}
-        </Route>
+        <Route path={SLUGS.login} component={Auth} />
+        <Route path={SLUGS.dashboard} component={Dashboard} />
       </Switch>
     </Router>
   );
